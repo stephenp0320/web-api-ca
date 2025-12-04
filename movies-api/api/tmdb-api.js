7d0a56e06ec6f21142620a7f39ea64c77c704bbb
stephenp0320 <stephen power>
2025-12-04 21:35:25 +0000

    Added movies router; get movies from TMDB

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export const getMovies = async () => {
     const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     );
 
     if (!response.ok) {
