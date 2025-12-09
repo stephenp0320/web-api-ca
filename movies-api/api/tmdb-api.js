37dfdafa6a32969b49df48969c4632f30fa1cace
stephenp0320 <stephen power>
2025-12-09 21:33:03 +0000

    added a handleTmdbResponce function for cleaner code

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,13 +1,22 @@
 import fetch from 'node-fetch';
 
+const handleTmdbResponce = async (response) => {
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.status_message || 'TMDB API error');    }
+    return await response.json();
+}
+
 export const getMovies = async () => {
     const response = await fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     );
+    return handleTmdbResponce(response);
+};
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.status_message || 'TMDB API error');    }
-
-    return await response.json();
+export const getWatchlist = async (id) => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    );
+    return handleTmdbResponce(response);
 };
