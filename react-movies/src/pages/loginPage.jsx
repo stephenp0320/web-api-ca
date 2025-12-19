8b609565f7ef56cf4d9fb0050a7a7b4d2b37fac6
stephenp0320 <stephen power>
2025-12-19 16:38:33 +0000

    updated error handling and login UI

--- react-movies/src/pages/loginPage.jsx
+++ react-movies/src/pages/loginPage.jsx
@@ -2,15 +2,22 @@ import { useContext, useState } from "react";
 import { Navigate, useLocation } from "react-router";
 import { MoviesContext } from '../contexts/moviesContext';
 import { Link } from "react-router";
+import { Box, Paper, Typography, TextField, Button, Stack, Alert } from "@mui/material";
 
 const LoginPage = () => {
     const context = useContext(MoviesContext);
 
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const login = () => {
-        context.authenticate(userName, password);
+
+    const login = async () => {
+        setError("") // clears any errors before login logic is ran
+        const res = await context.authenticate(userName, password);
+        if (!res) {
+            setError("Invalid username or password")
+        }
     };
 
     let location = useLocation();
@@ -23,20 +30,29 @@ const LoginPage = () => {
     }
 
     return (
-        <>
-            <h2>Login page</h2>
-            <p>You must log in to view the protected pages </p>
-            <input id="username" placeholder="user name" onChange={e => {
-                setUserName(e.target.value);
-            }}></input><br />
-            <input id="password" type="password" placeholder="password" onChange={e => {
-                setPassword(e.target.value);
-            }}></input><br />
-            {/* Login web form  */}
-            <button onClick={login}>Log in</button>
-            <p>Not Registered?
-                <Link to="/signup">Sign Up!</Link></p>
-        </>
+        //https://mui.com/material-ui/react-box/
+        <Box sx={{ minHeight: "70vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            {/* https://mui.com/material-ui/react-paper/ */}
+            <Paper sx={{ p: 4, width: 400 }}>
+                <Typography variant="h4" align="center" gutterBottom>
+                    Login
+                </Typography>
+
+                <Stack spacing={2}>
+                    {/* https://react.dev/learn/conditional-rendering */}
+                    {error && <Alert severity="error">{error}</Alert>}
+
+                    <TextField label="Username" value={userName} onChange={(e) => setUserName(e.target.value)}fullWidth/>
+
+                    <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} fullWidth/>
+
+                    <Button variant="contained" size="large" onClick={login}>
+                        Log In
+                    </Button>
+                    <Typography variant="body2" align="center"> Not registered? <Link to="/signup">Sign up</Link></Typography>
+                </Stack>
+            </Paper>
+        </Box>
     );
 };
 
