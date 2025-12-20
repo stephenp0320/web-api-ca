e5afc43cde39c67c19e1b3889e1df84646e989b4
stephenp0320 <stephen power>
2025-12-20 14:54:34 +0000

    Watchlist api routes added

---
+++ movies-api/api/watchlist/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import Watchlist from './watchlistModel';
+import asyncHandler from 'express-async-handler';
+import authenticate from '../../authenticate/index.js';
+
+const router = express.Router();
+router.use(authenticate); //protect the watchlist routes
+
+// get users
+router.get('/', async (req, res) => {
+    const items = await Watchlist.find({ username: req.user.username });
+    res.status(200).json(items);
+});
+
+router.post('/', asyncHandler(async (req, res) => {
+    const { movieID } = req.body;
+    if (!movieID) {
+        return res.status(400).json({ success: false, msg: 'MovieID required.' });
+    }
+    const created = await Watchlist.create({
+        username: req.body.username,
+        movieID,
+    });
+    res.status(201).json({ success: true, item: created });
+})
+);
+// delete added
+router.delete( "/:movieId", asyncHandler(async (req, res) => {
+      const movieId = Number(req.params.movieId);
+      await Watchlist.deleteOne({
+        username: req.user.username,
+        movieId,
+      });
+  
+      res.status(204).end();
+    })
+  );
+
+export default router;
