b4ddc052990dcc6b6792301f930857e42e95ba47
stephenp0320 <stephen power>
2025-12-20 14:23:36 +0000

    added Watchlist mongoose model

---
+++ movies-api/api/watchlist/watchlistModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const WatchlistSchema = new Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+},
+{timestamps: true}
+);
+// one movie per one user
+WatchlistSchema.index({ username: 1, movieId: 1 }, { unique: true });
+export default mongoose.model("Watchlist", WatchlistSchema);
