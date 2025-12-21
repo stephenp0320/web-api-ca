590d5dd767c304a5f49e65d980b208f4c2819d70
stephenp0320 <stephen power>
2025-12-21 14:23:23 +0000

    files formatted correctly

--- movies-api/api/watchlist/watchlistModel.js
+++ movies-api/api/watchlist/watchlistModel.js
@@ -6,7 +6,7 @@ const WatchlistSchema = new Schema({
     username: { type: String, required: true, index: true },
     movieId: { type: Number, required: true },
 },
-{timestamps: true}
+    { timestamps: true }
 );
 // one movie per one user
 WatchlistSchema.index({ username: 1, movieId: 1 }, { unique: true });
